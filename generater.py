7d1bb76de862fceca8a2561e9d1b82147b2bcdee
Nischal2017 <bknischal2010@gmail.com>
2022-06-22 00:42:52 +0530

    5 day test

--- generater.py
+++ generater.py
@@ -31,11 +31,4 @@ def makeCommits(day):
 	dateAndTime = datetime.utcfromtimestamp(time.time()+3600*5.5-(86400*day)).strftime('%Y-%m-%d %H:%M:%S')
 	data = generateRandomJson(dateAndTime)
 	result = writeIntoFile(file,data)
-	print(data["dateAndTime"],len(data),result,sep="\n")
 	return [data["dateAndTime"],len(data)]
-
-
-
-# os.system("git add .")
-# os.system('git commit --date="'+d+'" -m commit')
-# os.system('git push')
--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import os
 import generater as gen
 from matplotlib import pyplot as plt
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Date(YYYY-MM-DD HH:MM:SS)", "No of Commits"]
 
 dates = {}
 commits = {}
 
 plt.style.use('seaborn')
 
-for days in range(1,366):
+pltdata = gen.makeCommits(0)
+os.system("git add .")
+os.system('git commit -m "First Commit"')
+table.add_row(pltdata)
+
+for days in range(1,6):
 	
 	date = f'{days} days ago'
 	pltdata = gen.makeCommits(days)
+	table.add_row(pltdata)
 	if(pltdata[0][0:7] in dates.keys()):
 		dates[pltdata[0][0:7]] += pltdata[1]
 	else:
-		dates[pltdata[0][0:7]] = 0
+		dates[pltdata[0][0:7]] = pltdata[1]
 
+	os.system("git add .")
+	os.system('git commit --date="'+date+'" -m "Commit"')
+
+os.system("git push")
+print(table)
 
 plt.bar(dates.keys(),dates.values())
 plt.plot_date(dates.keys(),dates.values(), linestyle='solid')
+plt.xlabel("Months [YYYY-MM]")
+plt.ylabel("No of Commits")
+plt.title("Distribution of Commits")
 plt.show()
