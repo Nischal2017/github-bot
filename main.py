559e393e4b54e8c7d1bb14516dd048d03b7da81e
Nischal2017 <bknischal2010@gmail.com>
2022-06-22 00:58:05 +0530

    final test

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ os.system("git add .")
 os.system('git commit -m "First Commit"')
 table.add_row(pltdata)
 
-for days in range(1,6):
+for days in range(1,366):
 	
 	date = f'{days} days ago'
 	pltdata = gen.makeCommits(days)
