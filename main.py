8fd92baf780a929fc1caf579f27687a9b1bac19c
Nischal2017 <bknischal2010@gmail.com>
2022-06-22 02:03:08 +0530

    randomizer improvement iteration 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import generater as gen
 import random
+import time
 from matplotlib import pyplot as plt
 from prettytable import PrettyTable
 
@@ -12,32 +13,62 @@ commits = {}
 
 plt.style.use('seaborn')
 
-no_of_commits = int(input("Enter the number of commits to be made : "))
+no_of_commits = int(input("Enter the number of commits you want to make : "))
 
 pltdata = gen.makeCommits(0)
 os.system("git add .")
 os.system('git commit -m "First Commit"')
-table.add_row(pltdata)
+dates[pltdata[0][0:7]] = 1
 
-for times in range(1, no_of_commits):
+
+print("Making atleast 1 commit for each day of the year.\nYou have 5 seconds to abort before operation starts.\nPress Ctrl+C to abort.")
+time.sleep(5)
+
+for days in range(1, 368):
 	
-	days = random.randint(1, 368)
 	date = f'{days} days ago'
 	pltdata = gen.makeCommits(days)
-	table.add_row(pltdata)
+
 	if(pltdata[0][0:7] in dates.keys()):
-		dates[pltdata[0][0:7]] += pltdata[1]
+		dates[pltdata[0][0:7]] += 1
 	else:
-		dates[pltdata[0][0:7]] = pltdata[1]
+		dates[pltdata[0][0:7]] = 1
 
 	os.system("git add .")
 	os.system('git commit --date="'+date+'" -m "Commit"')
 
+print("Randomizing the number of commits on different days.\nDo not abort now!!")
+time.sleep(3)
+
+for days in range(1, no_of_commits-367):
+	
+	days = random.randint(1,368)
+	date = f'{days} days ago'
+	pltdata = gen.makeCommits(days)
+	
+	dates[pltdata[0][0:7]] += 1
+	
+	os.system("git add .")
+	os.system('git commit --date="'+date+'" -m "Commit"')
+
+for item in dates.items():
+	table.add_row(item)
+
 os.system("git push")
 print(table)
+print("Total commits made :" + str(sum(dates.values())))
 
-plt.bar(dates.keys(),dates.values())
-plt.plot_date(dates.keys(),dates.values(), linestyle='solid')
+print("Choose the which graph needs to be generated")
+print("Enter 1 for Bar Chart")
+print("Enter 2 for Line Chart")
+print("Enter 3 for a Bar/Line Chart")
+graph = int(input("Make your choice : "))
+
+if graph < 2 or graph > 2:
+	plt.bar(dates.keys(),dates.values())
+if graph > 1 or graph > 2:
+	plt.plot_date(dates.keys(),dates.values(), linestyle='solid')
+
 plt.xlabel("Months [YYYY-MM]")
 plt.ylabel("No of Commits")
 plt.title("Distribution of Commits")
